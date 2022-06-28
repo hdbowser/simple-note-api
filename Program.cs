289939ee2b42d8bb4df719d6246dab115514217e
Victor M. Brito <hdbowser@gmail.com>
2022-06-27 20:09:12 -0400

    ...

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ using SimpleNote.Data.Configurations;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
